bfa8297a1882e67e50023ec060f478226b209dc5
Sri Pallav <sripallavyerram@gmail.com>
2026-09-06 21:32:06 +0530

    code_on_06_09_2026

---
+++ AreaOfTriangle.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class AreaOfTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter height: ");
+        float height = sc.nextFloat();
+        System.out.print("enter breadth: ");
+        float breadth = sc.nextFloat();
+
+        Areacal aoft = new Areacal();
+        System.out.print(aoft.TriangleArea(height,breadth));
+
+
+
+        
+    }
+    
+}
+
+class Areacal{
+    public float TriangleArea(float height,float breadth){
+        float area = 0.5f * height * breadth;
+        return area;
+    }
+}
